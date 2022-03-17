de52eb692b9d76fd42ba7cc38dd6f59a11416e08
kanghyun98 <lkh981107@gmail.com>
2022-03-17 16:58:29 +0900

    feat: add constants for social login

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,8 @@
 export const DEV_SETTING = {
   port: '3065',
+  redirectURIGoogle: 'http://localhost:3000/auth/google/callback',
+  redirectURIKakao: 'http://localhost:3000/auth/kakao/callback',
+  redirectURIApple: 'http://localhost:3000/auth/apple/callback',
   db: {
     host: 'localhost',
     port: '3306',
@@ -11,6 +14,9 @@ export const DEV_SETTING = {
 export const PROD_SETTING = {
   port: '',
   url: 'https://sasil.com',
+  redirectURIGoogle: '',
+  redirectURIKakao: '',
+  redirectURIApple: '',
   db: {
     host: 'localhost',
     port: '3306',
