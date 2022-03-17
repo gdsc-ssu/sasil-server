aad392e3f68983bd225908653eecd1e402b5a0c2
이강현 <lkh981107@gmail.com>
2022-03-17 12:04:02 +0900

    feat: add settings about CORS, Session, Cookie for login (#8)
    * chore: add cookie-parser, cors, express-session packages
    
    * feat: add constants
    
    * feat: add settings about cors, cookie, session for login

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import 'reflect-metadata';
 import express from 'express';
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import { createConnection } from 'typeorm';
 
@@ -11,12 +14,38 @@ dotenv.config();
 const isProdMode: boolean = process.env.NODE_ENV === 'production';
 const env = isProdMode ? 'production' : 'development';
 
+// DB
 createConnection(ormconfig[env]).then(() => {
   console.log('DB Connection!');
 });
 
+// Express
 const app = express();
 app.set('port', isProdMode ? PROD_SETTING.port : DEV_SETTING.port);
+
+// CORS
+app.use(
+  cors({
+    origin: isProdMode ? PROD_SETTING.url : true, // Dev Mode에서는 전부 허용
+    credentials: true,
+  }),
+);
+
+// Cookie(sessionId 생성)
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+// Session
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET!,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: isProdMode, // Dev Mode에서는 http도 가능하게
+    },
+  }),
+);
 
 app.listen(app.get('port'), () => {
   console.log(`server is running on ${app.get('port')}`);
