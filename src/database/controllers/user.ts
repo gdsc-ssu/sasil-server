1d657df3013f12b313321e11aa6ec73ca5566a65
kanghyun98 <lkh981107@gmail.com>
2022-03-18 16:33:16 +0900

    feat: add controllers about getUser, addUser

--- src/database/controllers/user.ts
+++ src/database/controllers/user.ts
@@ -2,10 +2,41 @@ import { getRepository } from 'typeorm';
 
 import UserEntity from '@/database/entity/user';
 
-export const getUser = async (id: number) => {
-  const userRepository = getRepository(UserEntity);
-  const userInfo = await userRepository.findOne({ where: { id } });
-  return userInfo;
+type LoginTypes = 'kakao' | 'google' | 'apple';
+
+export const getUserDataById = async (id: number) => {
+  const userData = await getRepository(UserEntity)
+    .createQueryBuilder('user')
+    .where('user.id = :id', { id })
+    .getOne();
+
+  return userData;
 };
 
-export const addUser = async () => {}; // TODO: api 작업 시 추가
+export const getUserDataByEmailAndType = async (
+  email: string,
+  loginType: LoginTypes,
+) => {
+  const userData = await getRepository(UserEntity)
+    .createQueryBuilder('user')
+    .where('user.email = :email', { email })
+    .andWhere('user.login_type = :loginType', { loginType })
+    .getOne();
+
+  return userData;
+};
+
+export const addUser = async (email: string, loginType: LoginTypes) => {
+  const userRepository = getRepository(UserEntity);
+
+  const newUserData = userRepository.create({
+    email,
+    login_type: loginType,
+    nickname: 'test', // TODO: random 생성
+    profile: 'test', // TODO: random 생성
+  });
+
+  await userRepository.save(newUserData);
+
+  return newUserData;
+};
