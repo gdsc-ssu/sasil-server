6e17065591831edcf7b29dad70531659682a7efb
kanghyun98 <lkh981107@gmail.com>
2022-03-20 16:57:15 +0900

    feat: add getting user data when login success (basic)

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,11 +1,20 @@
 import express from 'express';
 
+import { checkLoggedin, checkLoggedout } from '@/routes/middlewares';
+
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.status(200).json({
-    hi: 'hello world',
-  });
+router.get('/me', checkLoggedin, (req, res) => {
+  if (req.user) {
+    res.status(200).json({ user: req.user });
+  }
+});
+
+router.post('/logout', checkLoggedin, (req, res) => {
+  req.logout();
+  if (req.session) {
+    req.session.destroy((e) => console.log(e));
+  }
 });
 
 export default router;
