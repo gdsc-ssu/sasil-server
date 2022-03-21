42554c0a7ce1e2e92d076f922d6aebbd080f5c30
kanghyun98 <lkh981107@gmail.com>
2022-03-21 21:03:53 +0900

    change cookie/session to jwt for social login

--- src/routes/middlewares.ts
+++ src/routes/middlewares.ts
@@ -1,25 +1,17 @@
+import passport from 'passport';
 import { Request, Response, NextFunction } from 'express';
 
-export const checkLoggedin = (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  if (req.isAuthenticated()) {
-    next();
-  } else {
-    res.status(401).send('로그인이 필요합니다.');
-  }
+const checkLoggedin = (req: Request, res: Response, next: NextFunction) => {
+  passport.authenticate('jwt', { session: false }, (err, user) => {
+    console.log(user);
+    if (user) {
+      console.log(user);
+      req.user = user;
+      next();
+    } else {
+      res.status(401).send('로그인이 필요합니다.');
+    }
+  })(req, res, next);
 };
 
-export const checkLoggedout = (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  if (!req.isAuthenticated()) {
-    next();
-  } else {
-    res.status(401).send('로그아웃이 필요합니다.');
-  }
-};
+export default checkLoggedin;
