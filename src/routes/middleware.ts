b2321d6576939f242d591d10fd802868f0e10cd1
kanghyun98 <lkh981107@gmail.com>
2022-03-22 09:32:41 +0900

    feat: add middleware for checking loggedIn with jwt

---
+++ src/routes/middleware.ts
@@ -0,0 +1,15 @@
+import passport from 'passport';
+import { Request, Response, NextFunction } from 'express';
+
+const checkLoggedin = (req: Request, res: Response, next: NextFunction) => {
+  passport.authenticate('jwt', { session: false }, (err, user) => {
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      res.status(401).send('로그인이 필요합니다.');
+    }
+  })(req, res, next);
+};
+
+export default checkLoggedin;
