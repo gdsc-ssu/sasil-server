3b014aa0a2981ad236929d71ee5f9ea0d38ad9f0
kanghyun98 <lkh981107@gmail.com>
2022-03-22 09:31:50 +0900

    feat: add a router that getting my basic info

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,11 +1,12 @@
 import express from 'express';
+import checkLoggedin from './middleware';
 
 const router = express.Router();
 
-router.get('/me', (req, res) => {
+router.get('/myInfo', checkLoggedin, async (req, res) => {
   try {
     if (req.user) {
-      res.status(200).json({ user: req.user });
+      res.status(200).json(req.user);
     } else {
       res.status(200).json(null);
     }
