fba88635596dd89f068e53f19ccebd1e787c583c
kanghyun98 <lkh981107@gmail.com>
2022-03-22 16:32:39 +0900

    refactor: change login api for scalability

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -5,13 +5,20 @@ import verifyGoogle from '@/auth/social/google';
 
 const router = express.Router();
 
-router.post('/login', async (req, res, next) => {
+router.post(`/login/:loginType`, async (req, res, next) => {
   try {
-    const userData = await verifyGoogle(req.body.token);
+    let userData;
+    if (req.params.loginType === 'google') {
+      userData = await verifyGoogle(req.body.token);
+    } else if (req.params.loginType === 'kakao') {
+      // userData = await verifyKakao(req.body.token);
+    }
+
     if (userData) {
       const token = makeJWTToken(userData.email, userData.login_type);
       return res.json({ token, userData });
     }
+
     return res.status(400).json({ message: '유저 확인 및 생성 오류' });
   } catch (error) {
     return next(error);
