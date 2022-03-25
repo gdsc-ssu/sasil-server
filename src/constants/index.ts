ed65a25841f94195d9dacaf223ac5c7be1eb7e42
kanghyun98 <lkh981107@gmail.com>
2022-03-25 20:59:06 +0900

    feat: change port from 3065,80 to 4000 for https

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,10 +1,10 @@
 export const DEV_SETTING = {
-  port: '3065',
+  port: '4000',
   clientURL: 'http://localhost:3000',
   redirectURI: {
-    google: 'http://localhost:3065/auth/google/callback',
-    kakao: 'http://localhost:3065/auth/kakao/callback',
-    apple: 'http://localhost:3065/auth/apple/callback',
+    google: 'http://localhost:4000/auth/google/callback',
+    kakao: 'http://localhost:4000/auth/kakao/callback',
+    apple: 'http://localhost:4000/auth/apple/callback',
   },
   db: {
     port: '3306',
@@ -14,7 +14,7 @@ export const DEV_SETTING = {
 
 // TODO 배포 모드 시 설정 추가
 export const PROD_SETTING = {
-  port: '80',
+  port: '4000',
   clientURL: 'https://sasil.app',
   redirectURI: {
     google: 'https://api.sasil.app/auth/google/callback',
