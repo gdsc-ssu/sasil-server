10d935e120cfd6abc48f10fbf5660d542aa0e3b1
Kanghyun Lee <lkh981107@gmail.com>
2022-07-03 19:19:33 +0900

    feat: add apis for post-detail-page (#31)
    * refactor: make bookmark relation table with separated code for efficiency
    
    * fix: change to the correct error code
    
    * chore: fix wrong comment
    
    * feat: add settings for post router
    
    * feat: add getPostData API
    
    * feat: add getComments API
    
    * feat: add writeComment API and rename variables
    
    * chore: change logging value to true in dev mode
    
    * fix: solve error by using wrong variable
    
    * feat: add deleteComment API
    
    * feat: add middleware for getting userId by access token
    
    * feat: add controllers for apis
    
    * feat: add routes code for apis
    
    * chore: add comments
    
    * chore: change schema name
    
    * chore: remove single quote in swagger file
    
    * chore: change comments
    
    * fix: add bookmarkCount to getReqPostByExpId api
    
    * chore: add swagger files

---
+++ src/database/entity/req-bookmark.ts
@@ -0,0 +1,25 @@
+/* eslint-disable import/no-cycle */
+import { Entity, ManyToOne, JoinColumn, PrimaryGeneratedColumn } from 'typeorm';
+
+import User from './user';
+import Request from './request';
+
+@Entity()
+class ReqBookmark {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @ManyToOne(() => User, (user) => user.reqBookmarks)
+  @JoinColumn({
+    name: 'user_id',
+  })
+  user!: User;
+
+  @ManyToOne(() => Request, (request) => request.reqBookmarks)
+  @JoinColumn({
+    name: 'req_id',
+  })
+  request!: Request;
+}
+
+export default ReqBookmark;
