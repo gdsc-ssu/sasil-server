9e746990edae44bacdf713782f90e502509f7a94
Kanghyun Lee <lkh981107@gmail.com>
2022-07-05 23:30:47 +0900

    feat: add apis for post-write-page (#34) (#35)
    * refactor: make category relation table with separated code for efficient
    
    * feat: add postWrite api
    
    * feat: add postWrite api swagger and refactor component structure
    
    * fix: solve getting categories data error
    
    * fix: change categories return value
    
    * refactor: change variables from snake case to camel case
    
    * refactor: makes middleware code simple
    
    * chore: rename swagger postWrite summary
    
    * feat: add getUploadImage api
    
    * fix: solve axios error by changing parameter structure
    
    * refactor: change return type for uploadImage api
    
    * chore: add swagger for imgUploadURL api

---
+++ src/database/entity/exp-category.ts
@@ -0,0 +1,25 @@
+/* eslint-disable import/no-cycle */
+import { Entity, ManyToOne, JoinColumn, PrimaryGeneratedColumn } from 'typeorm';
+
+import Cateogry from './category';
+import Experiment from './experiment';
+
+@Entity()
+class ExpCategory {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @ManyToOne(() => Cateogry, (category) => category.expCategories)
+  @JoinColumn({
+    name: 'category_id',
+  })
+  category!: Cateogry;
+
+  @ManyToOne(() => Experiment, (experiment) => experiment.expCategories)
+  @JoinColumn({
+    name: 'exp_id',
+  })
+  experiment!: Experiment;
+}
+
+export default ExpCategory;
