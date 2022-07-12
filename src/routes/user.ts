e6afdb0c2063c830401593dc80fa781f132dbcf9
kanghyun98 <lkh981107@gmail.com>
2022-07-12 16:07:43 +0900

    feat: add delete user with soft-delete

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,8 +1,9 @@
 import express, { Request, Response } from 'express';
 
 import wrapAsync from '@/utils/wrapAsync';
-import { getUserById } from '@/database/controllers/user';
+import { deleteUser, getUserById } from '@/database/controllers/user';
 import { checkLoggedIn } from './middleware';
+import { UnauthorizedError } from '@/errors/customErrors';
 
 const router = express.Router();
 
@@ -12,6 +13,22 @@ router.get(
   wrapAsync(async (req: Request, res: Response) => {
     const userData = await getUserById(req.userId!);
     return res.json(userData);
+  }),
+);
+
+router.delete(
+  '/me',
+  checkLoggedIn,
+  wrapAsync(async (req: Request, res: Response) => {
+    const { userId } = req;
+
+    if (!userId) {
+      throw new UnauthorizedError('로그인이 필요한 요청입니다.');
+    }
+
+    await deleteUser(userId);
+
+    res.end();
   }),
 );
 
