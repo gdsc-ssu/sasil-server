f17614e01d60b6439f4783c70e60f6b806b14fda
kanghyun98 <lkh981107@gmail.com>
2022-07-18 23:32:12 +0900

    feat: add getUserData by id

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,10 +3,11 @@ import express, { Request, Response } from 'express';
 import wrapAsync from '@/utils/wrapAsync';
 import { deleteUser, getUserById } from '@/database/controllers/user';
 import { checkLoggedIn } from './middleware';
-import { UnauthorizedError } from '@/errors/customErrors';
+import { BadRequestError, UnauthorizedError } from '@/errors/customErrors';
 
 const router = express.Router();
 
+// 내 정보 조회
 router.get(
   '/me',
   checkLoggedIn,
@@ -22,6 +23,7 @@ router.get(
   }),
 );
 
+// 회원 탈퇴
 router.delete(
   '/me',
   checkLoggedIn,
@@ -35,6 +37,21 @@ router.delete(
     await deleteUser(userId);
 
     res.end();
+  }),
+);
+
+// 유저 정보 조회
+router.get(
+  '/:id',
+  wrapAsync(async (req: Request, res: Response) => {
+    const userId = Number(req.params.id);
+
+    if (!userId) {
+      throw new BadRequestError('올바르지 않은 userId 값을 포함한 요청입니다.');
+    }
+
+    const userData = await getUserById(userId);
+    return res.json(userData);
   }),
 );
 
