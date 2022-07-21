dc256b97a750c7140e74007c9c3eb0e8962296e9
kanghyun98 <lkh981107@gmail.com>
2022-07-21 13:49:26 +0900

    feat: add searchByQuery api

--- src/database/controllers/search.ts
+++ src/database/controllers/search.ts
@@ -1,5 +1,7 @@
 import { getRepository } from 'typeorm';
 
+import ExperimentEntity from '@/database/entity/experiment';
+import RequestEntity from '@/database/entity/request';
 import CategoryEntity from '@/database/entity/category';
 import ReqCategoryEntity from '@/database/entity/req-category';
 import ExpCategoryEntity from '@/database/entity/exp-category';
@@ -86,6 +88,147 @@ export const searchExperimentListByTag = async (
 
   const result = experimentListByTag.map((expData) => {
     const { expCategories, content, ...data } = expData.experiment;
+
+    const categories = expCategories.map((categoryData) => ({
+      id: categoryData.category.id,
+      name: categoryData.category.name,
+    }));
+
+    return { ...data, categories };
+  });
+
+  return result;
+};
+
+/**
+ * 키워드(쿼리)를 이용한 의뢰 검색
+ *
+ * @param query 검색 키워드
+ * @param page page number
+ * @param display posts number
+ * @param sort 정렬 기준 (popular | recent)
+ * @returns request posts
+ */
+export const searchRequestListByQuery = async (
+  query: string,
+  page: number,
+  display: number,
+  sort: 'popular' | 'recent',
+) => {
+  const sortType = {
+    popular: {
+      first: 'likeCount',
+      second: 'topReqs.likeCount',
+    },
+    recent: {
+      first: 'subreq.createdAt',
+      second: 'request.createdAt',
+    },
+  };
+
+  const reqListData = await getRepository(RequestEntity)
+    .createQueryBuilder('request')
+    .select([
+      'request.id',
+      'request.createdAt',
+      'request.updatedAt',
+      'request.title',
+      'request.thumbnail',
+      'request.state',
+    ])
+    .innerJoin(
+      (qb) =>
+        qb
+          .select(['subreq.id', 'COUNT(likes.id) as likeCount'])
+          .from(RequestEntity, 'subreq')
+          .where('subreq.title like :query', { query: `%${query}%` })
+          .leftJoin('subreq.reqLikes', 'likes')
+          .groupBy('subreq.id')
+          .offset((page - 1) * display)
+          .limit(display)
+          .orderBy(sortType[sort].first, 'DESC'),
+      'topReqs',
+      'topReqs.subreq_id = request.id',
+    )
+    .leftJoinAndSelect('request.user', 'user')
+    .leftJoinAndSelect('request.reqCategories', 'reqCategories')
+    .leftJoinAndSelect('reqCategories.category', 'category')
+    .loadRelationCountAndMap('request.likeCount', 'request.reqLikes')
+    .orderBy(sortType[sort].second, 'DESC')
+    .getMany();
+
+  const result = reqListData.map((expData) => {
+    const { reqCategories, ...data } = expData;
+
+    const categories = reqCategories.map((categoryData) => ({
+      id: categoryData.category.id,
+      name: categoryData.category.name,
+    }));
+
+    return { ...data, categories };
+  });
+
+  return result;
+};
+
+/**
+ * 키워드(쿼리)를 이용한 실험 검색
+ *
+ * @param query 검색 키워드
+ * @param page page number
+ * @param display posts number
+ * @param sort 정렬 기준 (popular | recent)
+ * @returns request posts
+ */
+export const searchExperimentListByQuery = async (
+  query: string,
+  page: number,
+  display: number,
+  sort: 'popular' | 'recent',
+) => {
+  const sortType = {
+    popular: {
+      first: 'likeCount',
+      second: 'topExps.likeCount',
+    },
+    recent: {
+      first: 'subexp.createdAt',
+      second: 'experiment.createdAt',
+    },
+  };
+
+  const expListData = await getRepository(ExperimentEntity)
+    .createQueryBuilder('experiment')
+    .select([
+      'experiment.id',
+      'experiment.createdAt',
+      'experiment.updatedAt',
+      'experiment.title',
+      'experiment.thumbnail',
+    ])
+    .innerJoin(
+      (qb) =>
+        qb
+          .select(['subexp.id', 'COUNT(likes.id) as likeCount'])
+          .from(ExperimentEntity, 'subexp')
+          .where('subexp.title like :query', { query: `%${query}%` })
+          .leftJoin('subexp.expLikes', 'likes')
+          .groupBy('subexp.id')
+          .offset((page - 1) * display)
+          .limit(display)
+          .orderBy(sortType[sort].first, 'DESC'),
+      'topExps',
+      'topExps.subexp_id = experiment.id',
+    )
+    .leftJoinAndSelect('experiment.user', 'user')
+    .leftJoinAndSelect('experiment.expCategories', 'expCategories')
+    .leftJoinAndSelect('expCategories.category', 'category')
+    .loadRelationCountAndMap('experiment.likeCount', 'experiment.expLikes')
+    .orderBy(sortType[sort].second, 'DESC')
+    .getMany();
+
+  const result = expListData.map((expData) => {
+    const { expCategories, ...data } = expData;
 
     const categories = expCategories.map((categoryData) => ({
       id: categoryData.category.id,
--- src/routes/search.ts
+++ src/routes/search.ts
@@ -4,6 +4,8 @@ import wrapAsync from '@/utils/wrapAsync';
 import {
   searchRequestListByTag,
   searchExperimentListByTag,
+  searchRequestListByQuery,
+  searchExperimentListByQuery,
 } from '@/database/controllers/search';
 import { BadRequestError } from '@/errors/customErrors';
 
@@ -23,16 +25,53 @@ router.get(
       throw new BadRequestError('잘못된 태그를 포함한 요청입니다.');
     }
 
-    const getSearchPostList =
+    const searchPostListByTag =
       postType === 'request'
         ? searchRequestListByTag
         : searchExperimentListByTag;
 
     if (displayNum > 0 && pageNum > 0) {
-      const searchPostsData = await getSearchPostList(
+      const searchPostsData = await searchPostListByTag(
         tagName,
         pageNum,
         displayNum,
+      );
+      return res.json(searchPostsData);
+    }
+
+    throw new BadRequestError('올바르지 않은 query를 포함한 요청입니다.');
+  }),
+);
+
+// 키워드(쿼리)로 검색
+router.get(
+  '/query/:postType',
+  wrapAsync(async (req: Request, res: Response) => {
+    const { postType } = req.params;
+    const { display = '12', page = '1', sort = 'recent', query } = req.query;
+    const [displayNum, pageNum] = [Number(display), Number(page)];
+
+    const queryName = decodeURIComponent(query as string);
+
+    if (!queryName) {
+      throw new BadRequestError('잘못된 쿼리를 포함한 요청입니다.');
+    }
+
+    const searchPostListByQuery =
+      postType === 'request'
+        ? searchRequestListByQuery
+        : searchExperimentListByQuery;
+
+    if (
+      displayNum > 0 &&
+      pageNum > 0 &&
+      (sort === 'recent' || sort === 'popular')
+    ) {
+      const searchPostsData = await searchPostListByQuery(
+        queryName,
+        pageNum,
+        displayNum,
+        sort,
       );
       return res.json(searchPostsData);
     }
